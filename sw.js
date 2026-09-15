9cb473343d25174454519332c05a725bdeeb52e0
Alberto Fernández <132201161+afsenovilla@users.noreply.github.com>
2026-09-15 14:55:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Caché offline simple. Sube CACHE cuando publiques cambios. */
-var CACHE = "sala-examen-v2";
+var CACHE = "sala-examen-v3";
 var ASSETS = [
-  "./", "index.html", "assets/style.css?v=2", "assets/app.js?v=2",
+  "./", "index.html", "assets/style.css?v=3", "assets/app.js?v=3",
   "assets/icon.svg", "assets/icon-192.png", "assets/icon-512.png",
   "data/exams.json", "manifest.webmanifest"
 ];
